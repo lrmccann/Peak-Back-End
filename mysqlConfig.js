24536932452e683941ce5104b92fc030438b05b0
Logan McCAnn <loganrmccann@gmail.com>
2021-01-13 03:06:30 -0600

    server

---
+++ mysqlConfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+    host : "localhost",
+    database : "users",
+    userName : "peakAdmin",
+    passowrd : "Chandon401",
+    defaultSchema : "users",
+    tables : ["account_info" , "user_posts" , "user_comments"]
+};
