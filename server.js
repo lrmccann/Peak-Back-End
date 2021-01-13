63cafdc7404bdcc0bbd0149a5dfc73b3ad0a7910
Logan McCAnn <loganrmccann@gmail.com>
2021-01-13 15:07:33 -0600

    server

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static('public'));
 app.use(cors());
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin",  "*");
+    res.header("Access-Control-Allow-Origin",  "*" , "http://peak-blogspace.s3-website.us-east-2.amazonaws.com/");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Credentials", true);
