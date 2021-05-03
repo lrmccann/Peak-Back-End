5bd11df6bd8d180f8985e4b85ab210984a6874b8
Logan McCAnn <loganrmccann@gmail.com>
2021-05-02 19:17:14 -0500

    router changes

--- routes/routes.js
+++ routes/routes.js
@@ -1,60 +1,60 @@
-// const router = require('express').Router();
+const router = require('express').Router();
 
 
 module.exports = function(app) {
     const getController = require("../Controller/controller");
 
-    app.route('/account-info')
+    router.route('/account-info')
     .get(getController.getAllUsers)
     .post(getController.createNewUser)
     .delete(getController.deleteAccount);
 
-    app.route('/user-details/:id1')
+    router.route('/user-details/:id1')
     .get(getController.fetchUserInfo)
 
-    app.route("/account-info-login/:id1/:id2")
+    router.route("/account-info-login/:id1/:id2")
     .get(getController.authenticateUser);
 
-    app.route('/user-posts')
+    router.route('/user-posts')
     .get(getController.getAllPosts);
 
-    app.route('/post-views/:id1')
+    router.route('/post-views/:id1')
     .put(getController.addPostView);
 
 
     // id1 = id of post ; id2 = user id
-    app.route('/user-bookmarks/:id1/:id2')
+    router.route('/user-bookmarks/:id1/:id2')
     .put(getController.bookmarkNewPost)
     .delete(getController.removeBookmarkedPost);
 
     // id1 = user id
-    app.route('/user-all-bookmarks/:id1')
+    router.route('/user-all-bookmarks/:id1')
     .get(getController.getBookmarkedPosts);
 
-    app.route('/user-bookmarks-home/:id1')
+    router.route('/user-bookmarks-home/:id1')
     .get(getController.bookmarksForHome);
 
 
     // id1 = like + 1; id2 = post id; id3 = post title
-    app.route("/numOfLikesForPost/:id1/:id2/:id3")
+    router.route("/numOfLikesForPost/:id1/:id2/:id3")
     .put(getController.addLike);
 
-    app.route('/user-posts/:id1')
+    router.route('/user-posts/:id1')
     .get(getController.getAllInfoOnPost)
     .delete(getController.deleteUserPost);
 
-    app.route('/getUserPost/:id1')
+    router.route('/getUserPost/:id1')
     .get(getController.displayTopPosts);
 
-    app.route('/create-new-post')
+    router.route('/create-new-post')
     .post(getController.postNewBlog);
 
 
-    app.route('/user-comments/:id1')
+    router.route('/user-comments/:id1')
     .get(getController.displayTopComments)
     .post(getController.postNewComment)
     .delete(getController.deleteUserComment);
     
-    app.route('/user-comments/:id1')
+    router.route('/user-comments/:id1')
     .get(getController.loadAllCommentsForPost);
 };
