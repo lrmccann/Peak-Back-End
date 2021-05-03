b6c2e06d7cb6e459542e37f8bda9d5d08481eaee
Logan McCAnn <loganrmccann@gmail.com>
2021-05-02 19:29:26 -0500

    jaws config

--- Controller/jawsdbConfig.js
+++ Controller/jawsdbConfig.js
@@ -1,9 +1,8 @@
 module.exports = {
-    host : "hwr4wkxs079mtb19.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    host : "hwr4wkxs079mtb19.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
     database : "xj2vbbkogwll9zgl",
     userName : "abcmgygb3vzp12j8",
     passowrd : "liluc5l0j218a8nj",
-    defaultSchema : "users",
     insecureAuth : true,
     // tables : ["account_info" , "user_posts" , "user_comments"]
 };
