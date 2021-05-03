482564849cc76bbe8ae3ec49aedcc8ff4c6df396
Logan McCAnn <loganrmccann@gmail.com>
2021-05-02 19:22:30 -0500

    server

--- server.js
+++ server.js
@@ -1,24 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors");
 const routes = require("./routes/routes");
 const mysqlConfig = require("./Controller/mysqlConfig");
 const jawsdbConfig = require("./Controller/jawsdbConfig");
 const mysql = require("mysql");
 const app = express();
 const PORT = process.env.PORT || 3005;
+const cors = require("cors");
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-// const path = require('path');
-
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname+'/client/build/index.html'));
-//   });
-//   app.use(express.static(path.join(__dirname, 'client/build')));
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 
 app.use(express.static('public'));
+
+app.use(cors());
 
 
 app.use((req, res, next) => {
@@ -30,7 +25,13 @@ app.use((req, res, next) => {
     next();
   });
 
-  app.use(cors());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+    app.use(express.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+
+    app.use(routes);
+
 
   if(process.env.NODE_ENV === "production"){
   const startMysqlServer = async () => {
@@ -85,7 +86,7 @@ app.get('/' , (req , res) => {
     res.send("Welcome to Peak")
 })
 
-routes(app);
+// routes(app);
 
 app.listen(PORT);
 
