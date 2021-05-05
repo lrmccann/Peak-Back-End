125847e4d7df26f3ba4bd8191ee408b26feff22f
Logan McCAnn <loganrmccann@gmail.com>
2021-05-04 19:08:29 -0500

    still way to many routes but cleaned up some

--- routes/routes.js
+++ routes/routes.js
@@ -1,77 +1,8 @@
-// const router = require('express').Router();
-
-
-// // module.exports = function(app) {
-//     const getController = require("../Controller/controller");
-
-//     router.route('/account-info')
-//     // .get(getController.getAllUsers)
-//     .post(getController.createNewUser)
-//     .delete(getController.deleteAccount);
-
-//     router.route("/account-info-login/:id1/:id2")
-//     .get(getController.authenticateUser);
-
-//     router.route('/user-posts')
-//     .get(getController.getAllPosts);
-
-//     router.route('/post-views/:id1')
-//     .put(getController.addPostView);
-
-
-//     // id1 = id of post ; id2 = user id
-//     router.route('/user-bookmarks/:id1/:id2')
-//     .put(getController.bookmarkNewPost)
-//     .delete(getController.removeBookmarkedPost);
-
-//     // id1 = user id
-//     router.route('/user-all-bookmarks/:id1')
-//     .get(getController.getBookmarkedPosts);
-
-//     router.route('/user-bookmarks-home/:id1')
-//     .get(getController.bookmarksForHome);
-
-
-//     // id1 = like + 1; id2 = post id; id3 = post title
-//     router.route("/numOfLikesForPost/:id1/:id2/:id3")
-//     .put(getController.addLike);
-
-//     router.route('/user-posts/:id1')
-//     .get(getController.getAllInfoOnPost)
-//     .delete(getController.deleteUserPost);
-
-//     router.route('/getUserPost/:id1')
-//     .get(getController.displayTopPosts);
-
-//     router.route('/create-new-post')
-//     .post(getController.postNewBlog);
-
-
-//     router.route('/user-comments/:id1')
-//     .get(getController.displayTopComments)
-//     // .post(getController.postNewComment)
-//     .delete(getController.deleteUserComment);
-    
-//     router.route('/user-comments/:id1')
-//     .get(getController.loadAllCommentsForPost);
-
-//     router.route('/user-comments/:id1/:id2/:id3')
-//     .post(getController.postNewComment);
-
-//     router.route('/liked-posts/:id1')
-//     .get(getController.getLikedPosts);
-// // };
-// module.exports = router;
-
-
 const router = require('express').Router();
 
-
-// module.exports = function(app) {
     const getController = require("../Controller/controller");
 
     router.route('/account-info')
-    // .get(getController.getAllUsers)
     .post(getController.createNewUser)
     .delete(getController.deleteAccount);
 
@@ -84,21 +15,16 @@ const router = require('express').Router();
     router.route('/post-views/:id1')
     .put(getController.addPostView);
 
-
-    // id1 = id of post ; id2 = user id
     router.route('/user-bookmarks/:id1/:id2')
     .put(getController.bookmarkNewPost)
     .delete(getController.removeBookmarkedPost);
 
-    // id1 = user id
     router.route('/user-all-bookmarks/:id1')
     .get(getController.getBookmarkedPosts);
 
     router.route('/user-bookmarks-home/:id1')
     .get(getController.bookmarksForHome);
 
-
-    // id1 = like + 1; id2 = post id; id3 = post title
     router.route("/numOfLikesForPost/:id1/:id2/:id3")
     .put(getController.addLike);
 
@@ -112,10 +38,8 @@ const router = require('express').Router();
     router.route('/create-new-post')
     .post(getController.postNewBlog);
 
-
     router.route('/user-comments/:id1')
     .get(getController.displayTopComments)
-    // .post(getController.postNewComment)
     .delete(getController.deleteUserComment);
     
     router.route('/user-comments/:id1')
@@ -124,7 +48,7 @@ const router = require('express').Router();
     router.route('/user-comments-post/:id1/:id2/:id3')
     .post(getController.postNewComment);
 
-        router.route('/liked-posts/:id1')
+    router.route('/liked-posts/:id1')
     .get(getController.getLikedPosts);
-// };
+
 module.exports = router;
