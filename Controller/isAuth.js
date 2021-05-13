c6ced12d919bde90d51fcf9bb2f67ab7d82172bf
Logan McCAnn <loganrmccann@gmail.com>
2021-05-12 23:57:16 -0500

    cleaned up code for middleware

--- Controller/isAuth.js
+++ Controller/isAuth.js
@@ -1,18 +1,10 @@
 const jwt = require('jsonwebtoken');
 
-
 const { JWT_SIGNATURE } = process.env;
 
 (exports.authenticateToken = function (req, res, next) {
-  // console.log(req, "request here logan!!!")
-  // const authHeader = req.rawHeaders.find( ({ authToken }) => authToken === 'Authorization' );
   const authHeader = req.header('Authorization');
-  console.log(authHeader, "LOGAN HERE HERE")
-  // const token = authHeader && authHeader.split(' ')[1]
-  // console.log(token, "ALSO HERE LOGAN HERE")
-
   if (authHeader == null) return res.sendStatus(401)
-
   jwt.verify(authHeader, JWT_SIGNATURE ,
      (err, pass) => {
       if(err){
