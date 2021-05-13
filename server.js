dde51a32e42c94824c9087d1c5bdc9140dad88b0
Logan McCAnn <loganrmccann@gmail.com>
2021-05-13 15:19:03 -0500

    cont

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use((req, res, next) => {
     // res.setHeader("Access-Control-Allow-Headers" , 'Authorization');
     res.setHeader("Access-Control-Allow-Origin",  "*");
-    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With , Authorization");
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With" , "Authorization");
     res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
