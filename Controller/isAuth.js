f37a60eb37a6917d708075b12daf2c321084d985
Logan McCAnn <loganrmccann@gmail.com>
2021-05-13 16:22:10 -0500

    cont

--- Controller/isAuth.js
+++ Controller/isAuth.js
@@ -7,28 +7,29 @@ const { JWT_SIGNATURE } = process.env;
   // console.log(req.rawHeaders, "RAWWWWHEADDEERRSSSS")
   const authHeader = req.header('Authorization');
   console.log(authHeader, "OLD AUTH HEADER")
-  // if (authHeader == null) return res.sendStatus(401);
-  if(authHeader == null){
-    let newAuthHeader = req.header('authorization');
-    console.log(newAuthHeader, "NEW AUTH HEADERS")
-    jwt.verify(newAuthHeader, JWT_SIGNATURE ,
-      (err, pass) => {
-       if(err){
-         console.log(err, "error authenticating jwt IN 2");
-       }else{
-         console.log("Success!!! JWT authenticated IN 2" , pass);
-       }
-     next();
-   });
-  }else{
+
   jwt.verify(authHeader, JWT_SIGNATURE ,
      (err, pass) => {
       if(err){
-        console.log(err, "error authenticating jwt");
+            let newAuthHeader = req.header('authorization');
+              console.log(newAuthHeader, "NEW AUTH HEADERS");
+              jwt.verify(newAuthHeader, JWT_SIGNATURE ,
+                (err, pass) => {
+                 if(err){
+                   console.log(err, "error authenticating jwt IN 2");
+                   res.status(400).send("FAILED");
+                 }else{
+                   console.log("Success!!! JWT authenticated IN 2" , pass);
+                   res.status(200).send("PASSED SECOND TIME");
+                 }
+               next();
+             });
       }else{
         console.log("Success!!! JWT authenticated" , pass);
+        res.status(200).send("PASSED FIRST TIME")
       }
     next();
   });
 }
-})
+// }
+)
