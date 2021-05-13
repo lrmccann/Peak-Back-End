9be9ced5677c3020059a7a37ed938face0088887
Logan McCAnn <loganrmccann@gmail.com>
2021-05-12 23:16:40 -0500

    auth update

--- Controller/isAuth.js
+++ Controller/isAuth.js
@@ -4,9 +4,8 @@ const jwt = require('jsonwebtoken');
 const { JWT_SIGNATURE } = process.env;
 
 (exports.authenticateToken = function (req, res, next) {
-  console.log(req.rawHeaders.Authorization, "request here logan!!!")
-  console.log(req.rawHeaders, "request here TOOOOOO logan!!!")
-  const authHeader = req.headers['authorization']
+  console.log(req.rawHeaders['Authorization'], "request here logan!!!")
+  const authHeader = req.rawHeaders['Authorization']
   const token = authHeader && authHeader.split(' ')[1]
 
   if (token == null) return res.sendStatus(401)
