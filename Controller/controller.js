4ac10a6c7045174b5eb5f0628f3eb8cce034364e
Logan McCAnn <loganrmccann@gmail.com>
2021-05-14 10:55:06 -0500

    cont

--- Controller/controller.js
+++ Controller/controller.js
@@ -391,13 +391,10 @@ const getUserLikes = async (userId, myCallback) => {
           `UPDATE user_posts SET blog_likes = blog_likes + 1 WHERE id=${postId}`,
           (error, results) => {
             console.log(results, "results to add like");
-            if (error){
-              console.log("Error updating like num")
-              //  || results.changedRows === 0
-              //  )
-                // {
+            if(error) {
+              console.log("Error updating like num");
               res.status(400).send(error);
-            } else {
+            } else if(!error) {
               console.log("Updated like num")
               getUserLikes(userId, addLikeArr);
             }
