d37a9b5d0f8801245710240b52efe6973987d316
Logan McCAnn <loganrmccann@gmail.com>
2021-06-07 16:32:50 -0500

    cleaned up server and sql config

--- server.js
+++ server.js
@@ -25,51 +25,6 @@ app.use((req, res, next) => {
     app.use(express.urlencoded({ extended: true , limit: '20mb' }));
     app.use(express.json({extended: true, limit : '20mb'}));
 
-    // const checkMySqlServer = async () => {
-    //   if(process.env.NODE_ENV === "production"){
-    //   const startMysqlServer = async () => {
-    //     console.error('CONNECTING');
-    //     connection = mysql.createConnection(process.env.JAWSDB_URL);
-    //     await connection.connect(function(err){
-    //         if(err){
-    //             console.error('CONNECTION FAILED' , err.code);
-    //             // startMysqlServer();
-    //         }else{
-    //             console.error("CONNECTED PROD");
-    //         }
-    //     });
-    //     connection.on("error" , function(err) {
-    //         if(err.fatal){
-    //             throw err.fatal;
-    //             // startMysqlServer();
-    //         }
-    //     })
-    // }
-    // startMysqlServer();
-    //   }else if(process.env.NODE_ENV === 'development') {
-    //     const startMysqlServerLocalHost = async () => {
-    //       console.log(sqlConfig);
-    //      connection = mysql.createConnection(sqlConfig);
-    //     await connection.connect(function(err){
-    //         if (err) {
-    //             console.log(err , "please connect again")
-    //             // startMysqlServerLocalHost();
-    //         }else{
-    //             console.log("CONNECTED DEV")
-    //         }
-    //         connection.on('error' ,function(err) {
-    //             if(err.fatal){
-    //                 // startMysqlServerLocalHost();
-    //                 throw err.fatal;
-    //             }
-    //         })
-    //         });
-    // }
-    // startMysqlServerLocalHost();
-    //   }
-    // }
-    // checkMySqlServer();
-
     app.use(expressWinston.logger({
         transports: [
           new winston.transports.Console()
@@ -100,58 +55,8 @@ app.use((req, res, next) => {
         colorize: true, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
       }));
 
-    
-// const checkMySqlServer = async () => {
-//   if(process.env.NODE_ENV === "production"){
-//   const startMysqlServer = async () => {
-//     console.error('CONNECTING');
-//     connection = mysql.createConnection(process.env.JAWSDB_URL);
-//     await connection.connect(function(err){
-//         if(err){
-//             console.error('CONNECTION FAILED' , err.code);
-//             startMysqlServer();
-//         }else{
-//             console.error("CONNECTED PROD");
-//         }
-//     });
-//     connection.on("error" , function(err) {
-//         if(err.fatal){
-//             throw err.fatal;
-//             // startMysqlServer();
-//         }
-//     })
-// }
-// startMysqlServer();
-//   }else {
-//     const startMysqlServerLocalHost = async () => {
-//       console.log(sqlConfig);
-//      connection = mysql.createConnection(sqlConfig);
-//     await connection.connect(function(err){
-//         if (err) {
-//             console.log(err.stack , "please connect again")
-//             startMysqlServerLocalHost();
-//         }else{
-//             console.log("CONNECTED DEV")
-//         }
-//         connection.on('error' ,function(err) {
-//             if(err.fatal){
-//                 // startMysqlServerLocalHost();
-//                 throw err.fatal;
-//             }
-//         })
-//         });
-// }
-// startMysqlServerLocalHost();
-//   }
-// }
-// checkMySqlServer();
-
 app.get('/' , (req , res) => {
     res.send("Welcome to Peak")
 })
 
-// app.listen(PORT);
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-
-// console.log(`server started on ${PORT}`);
